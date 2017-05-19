1c0bf065ba1132f77657e2fe16f112328e579851
Aaron Boatwright <boatwrightaaron@gmail.com>
2017-05-19 14:25:27 -0700

    final push...finished game

--- public/js/platform.js
+++ public/js/platform.js
@@ -1,5 +1,6 @@
 var GAME_LEVELS = [
-  ["                                                                                ",
+  [
+    "                                                                                ",
     "                                                                                ",
     "                                                                                ",
     "                                                                                ",
@@ -24,7 +25,8 @@ var GAME_LEVELS = [
     "                                                                                ",
     "                                                                                "
   ],
-  ["                                      x!!x                        xxxxxxx                                    x!x  ",
+  [
+    "                                      x!!x                        xxxxxxx                                    x!x  ",
     "                                      x!!x                     xxxx     xxxx                                 x!x  ",
     "                                      x!!xxxxxxxxxx           xx           xx                                x!x  ",
     "                                      xx!!!!!!!!!!xx         xx             xx                               x!x  ",
@@ -55,7 +57,8 @@ var GAME_LEVELS = [
     "!!!!x x!!!!!!x         x!!!!!!xxxxxxxxx!!!!!!!!!!!!!!!!!!xx    !                                                  ",
     "!!!!x x!!!!!!x         x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!xx     !                                                  "
   ],
-  ["                                                                                                              ",
+  [
+    "                                                                                                              ",
     "                                                                                                              ",
     "                                                                                                              ",
     "                                                                                                              ",
@@ -103,7 +106,8 @@ var GAME_LEVELS = [
     "                                                                                                              ",
     "                                                                                                              "
   ],
-  ["                                                                                                  xxx x       ",
+  [
+    "                                                                                                  xxx x       ",
     "                                                                                                      x       ",
     "                                                                                                  xxxxx       ",
     "                                                                                                  x           ",
@@ -140,7 +144,6 @@ var GAME_LEVELS = [
   ]
 ];
 
-
 function Level(plan) {
   this.width = plan[0].length;
   this.height = plan.length;
@@ -154,11 +157,11 @@ function Level(plan) {
       var ch = line[x],
         fieldType = null;
       var Actor = actorChars[ch];
-      if (Actor)
+      if (Actor) 
         this.actors.push(new Actor(new Vector(x, y), ch));
-      else if (ch == "x")
+      else if (ch == "x") 
         fieldType = "wall";
-      else if (ch == "!")
+      else if (ch == "!") 
         fieldType = "lava";
       gridLine.push(fieldType);
     }
@@ -226,7 +229,8 @@ var simpleLevel = new Level(GAME_LEVELS);
 
 function elt(name, className) {
   var elt = document.createElement(name);
-  if (className) elt.className = className;
+  if (className) 
+    elt.className = className;
   return elt;
 }
 
@@ -257,8 +261,7 @@ DOMDisplay.prototype.drawBackground = function() {
 DOMDisplay.prototype.drawActors = function() {
   var wrap = elt("div");
   this.level.actors.forEach(function(actor) {
-    var rect = wrap.appendChild(elt("div",
-      "actor " + actor.type));
+    var rect = wrap.appendChild(elt("div", "actor " + actor.type));
     rect.style.width = actor.size.x * scale + "px";
     rect.style.height = actor.size.y * scale + "px";
     rect.style.left = actor.pos.x * scale + "px";
@@ -268,7 +271,7 @@ DOMDisplay.prototype.drawActors = function() {
 };
 
 DOMDisplay.prototype.drawFrame = function() {
-  if (this.actorLayer)
+  if (this.actorLayer) 
     this.wrap.removeChild(this.actorLayer);
   this.actorLayer = this.wrap.appendChild(this.drawActors());
   this.wrap.className = "game " + (this.level.status || "");
@@ -287,18 +290,17 @@ DOMDisplay.prototype.scrollPlayerIntoView = function() {
     bottom = top + height;
 
   var player = this.level.player;
-  var center = player.pos.plus(player.size.times(0.5))
-    .times(scale);
+  var center = player.pos.plus(player.size.times(0.5)).times(scale);
 
-  if (center.x < left + margin)
+  if (center.x < left + margin) 
     this.wrap.scrollLeft = center.x - margin;
-  else if (center.x > right - margin)
+  else if (center.x > right - margin) 
     this.wrap.scrollLeft = center.x + margin - width;
-  if (center.y < top + margin)
+  if (center.y < top + margin) 
     this.wrap.scrollTop = center.y - margin;
-  else if (center.y > bottom - margin)
+  else if (center.y > bottom - margin) 
     this.wrap.scrollTop = center.y + margin - height;
-};
+  };
 
 DOMDisplay.prototype.clear = function() {
   this.wrap.parentNode.removeChild(this.wrap);
@@ -310,36 +312,33 @@ Level.prototype.obstacleAt = function(pos, size) {
   var yStart = Math.floor(pos.y);
   var yEnd = Math.ceil(pos.y + size.y);
 
-  if (xStart < 0 || xEnd > this.width || yStart < 0)
+  if (xStart < 0 || xEnd > this.width || yStart < 0) 
     return "wall";
-  if (yEnd > this.height)
+  if (yEnd > this.height) 
     return "lava";
   for (var y = yStart; y < yEnd; y++) {
     for (var x = xStart; x < xEnd; x++) {
       var fieldType = this.grid[y][x];
-      if (fieldType) return fieldType;
+      if (fieldType) 
+        return fieldType;
+      }
     }
-  }
 };
 
 Level.prototype.actorAt = function(actor) {
   for (var i = 0; i < this.actors.length; i++) {
     var other = this.actors[i];
-    if (other != actor &&
-      actor.pos.x + actor.size.x > other.pos.x &&
-      actor.pos.x < other.pos.x + other.size.x &&
-      actor.pos.y + actor.size.y > other.pos.y &&
-      actor.pos.y < other.pos.y + other.size.y)
+    if (other != actor && actor.pos.x + actor.size.x > other.pos.x && actor.pos.x < other.pos.x + other.size.x && actor.pos.y + actor.size.y > other.pos.y && actor.pos.y < other.pos.y + other.size.y) 
       return other;
-  }
-};
+    }
+  };
 
 var maxStep = 0.05;
 
 Level.prototype.animate = function(step, keys) {
-  if (this.status != null)
+  if (this.status != null) 
     this.finishDelay -= step;
-
+  
   while (step > 0) {
     var thisStep = Math.min(step, maxStep);
     this.actors.forEach(function(actor) {
@@ -351,13 +350,14 @@ Level.prototype.animate = function(step, keys) {
 
 Lava.prototype.act = function(step, level) {
   var newPos = this.pos.plus(this.speed.times(step));
-  if (!level.obstacleAt(newPos, this.size))
+  if (!level.obstacleAt(newPos, this.size)) 
     this.pos = newPos;
-  else if (this.repeatPos)
+  else if (this.repeatPos) 
     this.pos = this.repeatPos;
-  else
+  else 
     this.speed = this.speed.times(-1);
-};
+  }
+;
 
 var wobbleSpeed = 8,
   wobbleDist = 0.07;
@@ -372,17 +372,20 @@ var playerXSpeed = 7;
 
 Player.prototype.moveX = function(step, level, keys) {
   this.speed.x = 0;
-  if (keys.left) this.speed.x -= playerXSpeed;
-  if (keys.right) this.speed.x += playerXSpeed;
-
+  if (keys.left) 
+    this.speed.x -= playerXSpeed;
+  if (keys.right) 
+    this.speed.x += playerXSpeed;
+  
   var motion = new Vector(this.speed.x * step, 0);
   var newPos = this.pos.plus(motion);
   var obstacle = level.obstacleAt(newPos, this.size);
-  if (obstacle)
+  if (obstacle) 
     level.playerTouched(obstacle);
-  else
+  else 
     this.pos = newPos;
-};
+  }
+;
 
 var gravity = 30;
 var jumpSpeed = 17;
@@ -394,11 +397,12 @@ Player.prototype.moveY = function(step, level, keys) {
   var obstacle = level.obstacleAt(newPos, this.size);
   if (obstacle) {
     level.playerTouched(obstacle);
-    if (keys.up && this.speed.y > 0)
+    if (keys.up && this.speed.y > 0) 
       this.speed.y = -jumpSpeed;
-    else
+    else 
       this.speed.y = 0;
-  } else {
+    }
+  else {
     this.pos = newPos;
   }
 };
@@ -408,9 +412,9 @@ Player.prototype.act = function(step, level, keys) {
   this.moveY(step, level, keys);
 
   var otherActor = level.actorAt(this);
-  if (otherActor)
+  if (otherActor) 
     level.playerTouched(otherActor.type, otherActor);
-
+  
   // Losing animation
   if (level.status == "lost") {
     this.pos.y += step;
@@ -427,8 +431,8 @@ Level.prototype.playerTouched = function(type, actor) {
       return other != actor;
     });
     if (!this.actors.some(function(actor) {
-        return actor.type == "coin";
-      })) {
+      return actor.type == "coin";
+    })) {
       this.status = "won";
       this.finishDelay = 1;
     }
@@ -466,9 +470,9 @@ function runAnimation(frameFunc) {
       stop = frameFunc(timeStep) === false;
     }
     lastTime = time;
-    if (!stop)
+    if (!stop) 
       requestAnimationFrame(frame);
-  }
+    }
   requestAnimationFrame(frame);
 }
 
@@ -481,7 +485,7 @@ function runLevel(level, Display, andThen) {
     display.drawFrame(step);
     if (level.isFinished()) {
       display.clear();
-      if (andThen)
+      if (andThen) 
         andThen(level.status);
       return false;
     }
